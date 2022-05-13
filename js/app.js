5aaaeb44bdac04060498cefcadf2e080f8da3688
jcryptodev <jdl.lourens@gmail.com>
2022-05-13 19:25:05 +0200

    added bar and doughnut chart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const closeButton = document.querySelector(".close");
 const alert = document.querySelector(".alert");
 const line = document.getElementById("line").getContext("2d");
+const bar = document.getElementById("bar").getContext("2d");
+const doughnut = document.getElementById("doughnut").getContext("2d");
 
 /* ALERT NOTIFICATION */
 closeButton.addEventListener("click", () => {
@@ -9,8 +11,84 @@ closeButton.addEventListener("click", () => {
 });
 
 /* CHART ONE */
-const myChart = new Chart(line, {
+const lineChart = new Chart(line, {
   type: "line",
+  data: {
+    labels: ["Q1", "Q2", "Q3", "Q4"],
+    datasets: [
+      {
+        data: [10, 15, 50, 100, 300, 400],
+        fill: true,
+        backgroundColor: "#7476bf52",
+        borderColor: ["#787bc9", "#787bc9", "#787bc9", "#787bc9"],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          display: true,
+          ticks: {
+            beginAtZero: true,
+            steps: 5,
+            stepValue: 10,
+            max: 100,
+          },
+        },
+      ],
+    },
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+/* CHART TWO */
+const barChart = new Chart(bar, {
+  type: "bar",
+  data: {
+    labels: ["Q1", "Q2", "Q3", "Q4"],
+    datasets: [
+      {
+        data: [10, 15, 50, 100, 300, 400],
+        fill: true,
+        backgroundColor: "#7476bf52",
+        borderColor: ["#787bc9", "#787bc9", "#787bc9", "#787bc9"],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          display: true,
+          ticks: {
+            beginAtZero: true,
+            steps: 5,
+            stepValue: 10,
+            max: 100,
+          },
+        },
+      ],
+    },
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+/* CHART THREE */
+const doughnutChart = new Chart(doughnut, {
+  type: "doughnut",
   data: {
     labels: ["Q1", "Q2", "Q3", "Q4"],
     datasets: [
