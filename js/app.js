08cfb6a3fd79ce1ed8e87627077ca1d389def289
jcryptodev <jdl.lourens@gmail.com>
2022-05-13 11:51:58 +0200

    added line chart

--- js/app.js
+++ js/app.js
@@ -1,7 +1,46 @@
 const closeButton = document.querySelector(".close");
 const alert = document.querySelector(".alert");
+const ctx = document.getElementById("myChart").getContext("2d");
 
 closeButton.addEventListener("click", () => {
   alert.style.visibility = "hidden";
   alert.style.display = "none";
+});
+
+/* CHART ONE */
+const myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["Q1", "Q2", "Q3", "Q4"],
+    datasets: [
+      {
+        data: [10, 15, 50, 100, 300, 400],
+        fill: true,
+        backgroundColor: "#7476bf52",
+        borderColor: ["#787bc9", "#787bc9", "#787bc9", "#787bc9"],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          display: true,
+          ticks: {
+            beginAtZero: true,
+            steps: 5,
+            stepValue: 10,
+            max: 100,
+          },
+        },
+      ],
+    },
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
 });
