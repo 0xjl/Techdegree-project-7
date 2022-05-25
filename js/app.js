32127ff42749e0edf3a5bb84080476f35b8620c2
jcryptodev <jdl.lourens@gmail.com>
2022-05-25 13:23:18 +0200

    fixed charts

--- js/app.js
+++ js/app.js
@@ -10,23 +10,150 @@ closeButton.addEventListener("click", () => {
   alert.style.display = "none";
 });
 
-/* traffic */
+let trafficData = {
+  labels: [
+    "16-22",
+    "23-29",
+    "30-5",
+    "6-12",
+    "13-19",
+    "20-26",
+    "27-3",
+    "4-10",
+    "11-17",
+    "18-24",
+    "25-31",
+  ],
+  datasets: [
+    {
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      backgroundColor: "rgba(116, 119, 191, .3)",
+      borderWidth: 1,
+    },
+  ],
+};
+
+let trafficOptions = {
+  backgroundColor: "rgba(112, 104, 201, .5)",
+  fill: true,
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0,
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
+let trafficChart = new Chart(line, {
+  type: "line",
+  data: trafficData,
+  options: trafficOptions,
+});
+
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [
+    {
+      label: "# of Hits",
+      data: [75, 115, 175, 125, 225, 200, 100],
+      backgroundColor: "#7477BF",
+      borderWidth: 1,
+    },
+  ],
+};
+
+const dailyOptions = {
+  aspectRatio: 2.1,
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
+let dailyChart = new Chart(bar, {
+  type: "bar",
+  data: dailyData,
+  options: dailyOptions,
+});
+
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [
+    {
+      label: "# of Users",
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: ["#7477BF", "#78CF82", "#51B6C8"],
+    },
+  ],
+};
+
+const mobileOptions = {
+  aspectRatio: 2.1,
+  plugins: {
+    legend: {
+      position: "right",
+      labels: {
+        boxWidth: 20,
+        fontStyle: "bold",
+      },
+    },
+  },
+};
+
+let mobileChart = new Chart(doughnut, {
+  type: "doughnut",
+  data: mobileData,
+  options: mobileOptions,
+});
+
+/* traffic 
 const lineChart = new Chart(line, {
   type: "line",
   data: {
-    labels: ["Q1", "Q2", "Q3", "Q4"],
+    labels: [
+      "16-22",
+      "23-29",
+      "30-5",
+      "6-12",
+      "13-19",
+      "20-26",
+      "27-3",
+      "4-10",
+      "11-17",
+      "18-24",
+      "25-31",
+    ],
     datasets: [
       {
-        data: [10, 15, 50, 100, 300, 400],
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
         fill: true,
-        backgroundColor: "#7476bf52",
-        borderColor: ["#787bc9", "#787bc9", "#787bc9", "#787bc9"],
+        backgroundColor: "rgba(116, 119, 191, .3)",
         borderWidth: 1,
       },
     ],
   },
   options: {
-    maintainAspectRatio: false,
+    backgroundColor: "rgba(112, 104, 201, .5)",
+    fill: true,
+    aspectRatio: 2.5,
+    animation: {
+      duration: 0,
+    },
     plugins: {
       legend: {
         display: false,
@@ -35,11 +162,14 @@ const lineChart = new Chart(line, {
   },
 });
 
-/* CHART TWO */
+*/
+
+/* CHART TWO 
 const barChart = new Chart(bar, {
   type: "bar",
   data: {
     labels: ["Q1", "Q2", "Q3", "Q4"],
+    aspectRatio: 2.5,
     datasets: [
       {
         data: [10, 15, 50, 100, 300, 400],
@@ -51,7 +181,6 @@ const barChart = new Chart(bar, {
     ],
   },
   options: {
-    maintainAspectRatio: false,
     plugins: {
       legend: {
         display: false,
@@ -59,8 +188,9 @@ const barChart = new Chart(bar, {
     },
   },
 });
+*/
 
-/* CHART THREE */
+/* CHART THREE 
 const doughnutChart = new Chart(doughnut, {
   type: "doughnut",
   data: {
@@ -76,7 +206,6 @@ const doughnutChart = new Chart(doughnut, {
     ],
   },
   options: {
-    maintainAspectRatio: false,
     plugins: {
       legend: {
         display: false,
@@ -84,3 +213,4 @@ const doughnutChart = new Chart(doughnut, {
     },
   },
 });
+*/
